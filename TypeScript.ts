9d52f2fcb678d67fbc52f53284bca759b1142144
nikakvinikadze <nikakvinikadze2007@gmail.com>
2024-11-15 22:08:27 +0400

    typescript part

--- TypeScript.ts
+++ TypeScript.ts
@@ -2,80 +2,130 @@
 
 // მაგალითი 1
 
-let name = "Alice";
+let name1: string = 'Alice';
 
 // მაგალითი 2
 
-let numbers = [1, 2, 3];
+let numbers: number[] = [1, 2, 3];
 
 // მაგალითი 3
 
-function multiply(a, b) {
+function multiply(a: number, b: number): number {
   return a * b;
 }
 
 // მაგალითი 4
+type User1 = {
+  id: number;
+  name: string;
+};
 
-const user = { id: 1, name: "Alice" };
+const user2: User1 = { id: 1, name: 'Alice' };
 
 // მაგალითი 5
 // რისთვის ვიყენებთ არსებული კოდში = Guest-ს :
 
-function greet(name: string = "Guest") {}
+function greet(name: string = 'Guest') {}
 
-// პასუხი:
+// პასუხი:tu ar shemova name mashin misi default value iqneba guest
 
 // მაგალითი 6
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type Config = {
 
-const config1 = {
-  theme: "dark",
+const config1: Config1 = {
+  theme: 'dark',
   options: {
     fontSize: 16,
     layout: null,
   },
 };
-
-const config2 = {
-  theme: "dark",
+type Config1 = {
+  theme: string;
+  options: {
+    fontSize: number;
+    layout: null;
+  };
 };
 
-const config3 = {
-  tester: "test",
+const config2: Config2 = {
+  theme: 'dark',
+};
+
+type Config2 = {
+  theme: string;
+};
+
+type Config3 = {
+  tester: string;
+};
+
+const config3: Config3 = {
+  tester: 'test',
 };
 
 // მაგალითი 7
-// დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
-const array1 = [42, "hello", { name: "Alice" }];
-const array2 = ["apple", true, { isValid: false }];
+// დაწერეთ ტიპი რომელიც მოtერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
+type Name = {
+  name: string;
+};
+
+const array1: (string | number | Name)[] = [42, 'hello', { name: 'Alice' }];
+
+type valid = {
+  isValid: boolean;
+};
+
+const array2: (string | boolean | valid)[] = [
+  'apple',
+  true,
+  { isValid: false },
+];
+
 const array3 = [];
 
 // მაგალითი 8
 // აღწერეთ რისი ტიპიზაცია ხდება არსებულ კოდში წერილობით
 
 type Handler = {
-  process?: () => string;
-  validate?: (input: string) => boolean;
-  log?: () => void;
+  process?: () => string; //proces tu arsebobs abrunebs strings
+  validate?: (input: string) => boolean; //igebs cvlads input type string da abrunebs boleans
+  log?: () => void; //aris void function
 };
 
 // მაგალითი 9
 
 // შექმენით ტიპი მონაცემისთივს:
 
-type User = {};
+type User = {
+  id: number;
+  username: string;
+  isAdmin: boolean;
+  profile: {
+    fullName: string;
+    age: number;
+    interests: string[];
+  };
+  settings: {
+    theme: string;
+    notifications: {
+      email: boolean;
+      sms: boolean;
+    };
+  };
+  metadata: undefined;
+};
 
 const user: User = {
   id: 101,
-  username: "tech_learner",
+  username: 'tech_learner',
   isAdmin: false,
   profile: {
-    fullName: "Alice Johnson",
+    fullName: 'Alice Johnson',
     age: 25,
-    interests: ["coding", "gaming", "reading"],
+    interests: ['coding', 'gaming', 'reading'],
   },
   settings: {
-    theme: "dark",
+    theme: 'dark',
     notifications: {
       email: true,
       sms: false,
